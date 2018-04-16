3a9e08e60c5374c8fd9cfdf96e041a0535d77fb8
Ben Mezger <me@benmezger.nl>
2018-04-15 21:04:45 -0300

    Copy kernel config.

--- copy.sh
+++ copy.sh
@@ -2,3 +2,4 @@ cp -rfv /etc/portage/* ./etc/portage/
 cp -rfv /etc/layman/* ./etc/layman/
 cp -rfv /etc/default/* /etc/default/
 cp -rfv /etc/sysctl.conf ./etc/sysctl.conf
+cp -rfv /usr/src/linux/.config ./.config
