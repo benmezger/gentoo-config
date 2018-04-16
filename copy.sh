e1020c06f6b9ab0407a1627a00d2e4b043dab940
Ben Mezger <me@benmezger.nl>
2018-04-15 21:11:47 -0300

    Added world file which contains all installed apps.

--- copy.sh
+++ copy.sh
@@ -3,3 +3,4 @@ cp -rfv /etc/layman/* ./etc/layman/
 cp -rfv /etc/default/* /etc/default/
 cp -rfv /etc/sysctl.conf ./etc/sysctl.conf
 cp -rfv /usr/src/linux/.config ./.config
+cp -rfv /var/lib/portage/world ./var/lib/portage/world
