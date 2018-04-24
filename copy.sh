4757394b78dcec1784214bd3aac4eb40fbb9df90
Ben Mezger <me@benmezger.nl>
2018-04-24 18:33:15 -0300

    Don't use -r for files.

--- copy.sh
+++ copy.sh
@@ -1,6 +1,8 @@
 cp -rfv /etc/portage/* ./etc/portage/
 cp -rfv /etc/layman/* ./etc/layman/
 cp -rfv /etc/default/* /etc/default/
-cp -rfv /etc/sysctl.conf ./etc/sysctl.conf
-cp -rfv /usr/src/linux/.config ./.config
-cp -rfv /var/lib/portage/world ./var/lib/portage/world
+
+
+cp -fv /etc/sysctl.conf ./etc/sysctl.conf
+cp -fv /usr/src/linux/.config ./.config
+cp -fv /var/lib/portage/world ./var/lib/portage/world
