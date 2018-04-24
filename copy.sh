6633f9a9eb7faaa0f95ab69f2dbb8ca6454f205e
Ben Mezger <me@benmezger.nl>
2018-04-24 18:38:31 -0300

    Copy genkernel.conf and slim.conf.

--- copy.sh
+++ copy.sh
@@ -2,7 +2,8 @@ cp -rfv /etc/portage/* ./etc/portage/
 cp -rfv /etc/layman/* ./etc/layman/
 cp -rfv /etc/default/* /etc/default/
 
-
+cp -fv /etc/genkernel.conf ./etc/genkernel.conf
+cp -fv /etc/slim.conf ./etc/slim.conf
 cp -fv /etc/sysctl.conf ./etc/sysctl.conf
 cp -fv /usr/src/linux/.config ./.config
 cp -fv /var/lib/portage/world ./var/lib/portage/world
